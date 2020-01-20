d4a4d154f1f0a0471ca2100eab77e6f985935038
grayluker05 <graysonluker@gmail.com>
2020-01-20 15:36:10 -0700

    authentication styling done

--- src/Components/Authentication/Authentication.js
+++ src/Components/Authentication/Authentication.js
@@ -4,6 +4,7 @@ import useInput from '../../hooks/useInput';
 import axios from 'axios';
 import {connect} from 'react-redux';
 import {getEmployee} from '../../redux/reducers/employeeReducer';
+import MonetaLogo from '../../img/MonetaLogo.png';
 
 const Authentication = (props) => {
     const [username, bindUsername, resetUsername] = useInput('');
@@ -12,7 +13,7 @@ const Authentication = (props) => {
     let login = () => {
         axios.post('/api/auth/login', {username: username, password: password}).then(res => {
             props.getEmployee(res.data);
-            //props.history.push('/')
+            props.history.push('/checkout')
             resetUsername()
             resetPassword()
         })
@@ -20,22 +21,26 @@ const Authentication = (props) => {
 
     return (
         <div className='auth'>
+            <img src={MonetaLogo} alt='logo' height='200px'/>
             <div className='auth-container'>
                 <div className='welcome'>
-                    <h1>Welcome back to Moneta!</h1>
+                    <div>Welcome back to Moneta!</div>
+                    <h2>Log in to get started<i className='right'></i></h2>
                 </div>
                 <div className='login'>
                     <h1>sign in</h1>
-                    <input 
-                        {...bindUsername}
-                        type='text'
-                        placeholder='username'
-                    />
-                    <input 
-                        {...bindPassword}
-                        type='password'
-                        placeholder='password'
-                    />
+                    <div>
+                        <input 
+                            {...bindUsername}
+                            type='text'
+                            placeholder='username'
+                        />
+                        <input 
+                            {...bindPassword}
+                            type='password'
+                            placeholder='password'
+                        />
+                    </div>
                     <button onClick={login}>login</button>
                 </div>
             </div>
