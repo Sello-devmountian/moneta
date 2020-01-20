535e35f28fe4fa6894c5a6068cdfdd871318bd6b
spiltmiruku <spiltmiruku@gmail.com>
2020-01-20 13:41:13 -0700

    Added reset.css; Added styling using scss;

--- src/Components/Admin/AddEmployee.js
+++ src/Components/Admin/AddEmployee.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import useInput from '../../hooks/useInput';
-
+import './addemployee.scss';
 
 function AddEmployee() {
     const [username, bindUserName, resetUsername, password, bindPassword, resetPassword, setNewEmployee] = useInput('');
     
     return (
-        <div>
-        <input {...bindUserName}></input>
-        <input {...bindPassword}></input>
-        <div onClick={(e) => setNewEmployee(e.target.value)}></div>
+        <div className='add-employee-box'>
+        <h1 className='new-employee-title'>add new employee</h1>
+        <input placeholder='username' {...bindUserName}></input>
+        <input placeholder='password' {...bindPassword}></input>
+        <div className='add-employee-btn' onClick={(e) => setNewEmployee(e.target.value)}>add employee</div>
         </div>
     );
 }
