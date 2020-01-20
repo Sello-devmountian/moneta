b6fe0cc8b19740a4cbf67b6e3bccb4b2182ccc91
spiltmiruku <spiltmiruku@gmail.com>
2020-01-20 11:51:25 -0700

    Added amin routes; Added file structure for Admin page; Added basic admin styling;

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,29 @@ import Customers from './Components/Customers/Customers';
 import Checkout from './Components/Employee/Checkout/Checkout'; 
 import Transactions from './Components/Admin/Transactions'; 
 
+import Admin from './Components/Admin/Admin';
+import AddProduct from './Components/Admin/AddProduct';
+import EditProducts from './Components/Admin/EditProducts';
+import DeleteProduct from './Components/Admin/DeleteProduct';
+import AddEmployee from './Components/Admin/AddEmployee';
+import DeleteEmployee from './Components/Admin/DeleteEmployee';
+
+
 export default (
     <Switch>
         <Route exact path='/' component={Authentication}/>
         <Route path='/customers' component={Customers} />
         <Route path='/checkout' component={Checkout} />
         <Route path='/transactions' component={Transactions} />
-        
+
+        {/* ----------ADMIN PAGE ROUTES---------- */}
+
+        <Route path='/admin' component={Admin} />
+
+        <Route path='/addproduct' component={AddProduct} />
+        <Route path='/editproducts' component={EditProducts} />
+        <Route path='/deleteproduct' component={DeleteProduct} />
+        <Route path='/addemployee' component={AddEmployee} />
+        <Route path='/deleteemployee' component={DeleteEmployee} />
     </Switch>
 );
