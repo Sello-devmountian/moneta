4cd80c9296f1f194bcb447a944966fa815896b63
James Nelson <johndoe@example.com>
2020-01-21 12:18:42 -0700

    Customer page updated, back end code added

--- src/Components/Customers/Customers.js
+++ src/Components/Customers/Customers.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import axios from 'axios'; 
 import {getCustomer} from '../../redux/reducers/customerReducer'; 
 import {connect} from 'react-redux'; 
@@ -7,6 +7,9 @@ import  './customers.css';
 
 
 const Customers = (props) => {
+    const [editUser, setEditUser] = useState(false);
+    //onClick={() => setEditUser(!editUser)}
+    // {editUser ? () : ()}
 
     useEffect(() => {
         getCustomer()
@@ -17,21 +20,85 @@ const Customers = (props) => {
             props.getCustomer(res.data)
         })
     }
+
+   
     
-    console.log(props.customer); 
+    console.log(props); 
+    console.log(useState); 
     return(
         <div className='customers-page'>
-            <div>
-               {props.customer.customer[0] && props.customer.customer.map(customers => {
-                   return(
-                       <div className='customers-table'>
-                           <div>{customers.email}</div>
-                           <div>{customers.phone}</div>
-                           <div>{customers.first_name}</div>
-                           <div>{customers.last_name}</div>
-                       </div>
-                   )
-               } )}
+            <div className='customers-table'>
+                
+                <div>
+                    <div className='table-info'>
+                        Email
+                    </div>
+                    {props.customer.customer[0] && props.customer.customer.map(customers => {
+                        return(
+                            <div className='customer-info'>
+                                <div>{customers.email}</div>
+                            </div>
+                        )
+                    } )}
+                </div>
+
+               <div>
+                    <div className='table-info'>
+                        Phone Number
+                    </div>
+                    {props.customer.customer[0] && props.customer.customer.map(customers => {
+                        return(
+                            <div className='customer-info'>
+                                <div>{customers.phone}</div>
+                            </div>
+                        )
+                    } )}
+                </div>
+
+                <div>
+                    <div className='table-info'>
+                        First Name
+                    </div>
+                    {props.customer.customer[0] && props.customer.customer.map(customers => {
+                        return(
+                            <div className='customer-info'>
+                                <div>{customers.first_name}</div>
+                            </div>
+                        )
+                    } )}
+                </div>
+
+                <div>
+                    <div className='table-info'>
+                        Last Name
+                    </div>
+                    
+                        {props.customer.customer[0] && props.customer.customer.map(customers => {
+                            return(
+                                <div className='customer-info'>
+                                    <div>{customers.last_name}</div>
+                                    
+                                </div>
+                                
+
+
+                            )
+                        } )}  
+                </div>
+
+                <div>
+                    <div className='table-info'>
+                        Edit
+                    </div>
+                    {props.customer.customer[0] && props.customer.customer.map(customers => {
+                        return(
+                            <div className='customer-info'>
+                    <button className='edit-button' onClick={() => setEditUser(!editUser)}>Customer ID:{customers.c_id}</button>
+                            </div>
+                        )
+                    } )}
+                </div>
+               
             </div>
            
         </div>
