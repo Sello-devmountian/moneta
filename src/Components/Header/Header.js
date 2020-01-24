88bc99a1415c7c0c454c3bc3cdd2eb3d191d6e1a
dpouls <dallinpoulson@gmail.com>
2020-01-24 13:55:42 -0700

    logout button, table done and sorted.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';  
 import {Link} from 'react-router-dom'; 
 import './header.css'
+import Axios from 'axios';
 
 
 const Header = (props) => {
+    const logout = () => {
+        Axios.post('/api/auth/logout')
+    }
 
     return(
         <header className='nav-bar'>
             <div className='nav-buttons'>
-                <Link className='nav-button' to='/customers'>Customers</Link>
                 <Link className='nav-button' to='/checkout'>Checkout</Link>
+                <Link className='nav-button' to='/customers'>Customers</Link>
                 <Link className='nav-button' to='/transactions'>Transactions</Link>
+                <Link onClick={logout} className='nav-button' to='/'>Log Out</Link>
             </div>
         </header>
     )
