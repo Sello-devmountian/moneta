08fa07942755a21ec218642b223b6072a2c7f0a6
grayluker05 <graysonluker@gmail.com>
2020-01-24 13:56:10 -0700

    made a changeModal

---
+++ src/Components/Employee/Payment/Cash/Change/Change.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './change.scss';
+
+const Change = () => {
+    return (
+        <div>
+            Change Modal
+        </div>
+    )
+}
+
+export default Change;
