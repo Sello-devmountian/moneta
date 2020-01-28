7635be5d02787cbb376f0bfaa7c22bc8f271a40a
spiltmiruku <spiltmiruku@gmail.com>
2020-01-27 18:53:41 -0700

    Minor css tweaks to authentication page, added ternary to conditionally display ‘admin’ link on the header.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';  
 import {Link } from 'react-router-dom'; 
+import { connect } from 'react-redux';
 import './header.css'
 import axios from 'axios';
 
@@ -9,17 +10,26 @@ const Header = (props) => {
         axios.post('/api/auth/logout')
     }
 
+    console.log(props)
+
     return(
         <header className='nav-bar'>
             <div className='nav-buttons'>
+            {props.employee.employee.is_admin ? 
+                <Link className='nav-button' to='/admin'>Admin</Link>
+                : null
+            }
                 <Link className='nav-button' to='/checkout'>Checkout</Link>
                 <Link className='nav-button' to='/customers'>Customers</Link>
                 <Link className='nav-button' to='/transactions'>Transactions</Link> 
-                <Link className='nav-button' to='/admin'>Admin</Link>
                 <Link onClick={logout} className='nav-button' to='/'>Log Out</Link>
             </div>
         </header>
     )
 }
 
-export default Header; 
+const mapStateToProps = reduxState => {
+    return reduxState;
+  };
+
+export default connect(mapStateToProps)(Header); 
