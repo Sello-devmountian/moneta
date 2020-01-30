3ab0f53c3fa9f9575f6b36698579b8703c38b4ac
spiltmiruku <spiltmiruku@gmail.com>
2020-01-30 12:27:43 -0700

    Styled all admin pages; Toggling product availability now working

--- src/Components/Admin/AddProduct.js
+++ src/Components/Admin/AddProduct.js
@@ -8,9 +8,8 @@ function AddProduct() {
   const [p_image, bindP_image, resetP_image] = useInput("");
   const [price, bindPrice, resetPrice] = useInput("");
   const [p_type, setType] = useState("scoops");
-  const [available, bindAvailable] = useState(false);
-  
-  
+  const [available, setAvailable] = useState(false);
+
   let setNewProduct = () => {
     axios
       .post("/api/products", {
@@ -20,56 +19,67 @@ function AddProduct() {
         p_type,
         available
       })
-      .then((response) => {
-        console.log(response, 'response')
+      .then(response => {
+        console.log(response, "response");
         alert("New product added");
-        
       })
       .catch(err => {
         console.log("Product not added", err);
       });
   };
 
-
   return (
     <div className="add-product-box">
       <h1 className="new-product-title">add new product</h1>
-      <section className='new-product-inputs'>
-
-      <input placeholder="name" {...bindName} type='text'/>
-      <input placeholder="image url" {...bindP_image} type='url'/>
-      <input placeholder="price" {...bindPrice} type='number'/>
-      <select
-        onChange={(e) => setType(e.target.value)}
-        className="product-type-dropdown"
-        value={p_type}
+      <section className="new-product-inputs">
+        <input placeholder="name" {...bindName} type="text" />
+        <input placeholder="image url" {...bindP_image} type="url" />
+        <input placeholder="price" {...bindPrice} type="number" />
+        <select
+          onChange={e => setType(e.target.value)}
+          className="product-type-dropdown"
+          value={p_type}
         >
-        <option value="scoops" selected>scoops</option>
-        <option value="soft-serve">soft-serve</option>
-        <option value="toppings">toppings</option>
-        <option value="cones">cones</option>
-      </select>
+          <option value="scoops" selected>
+            scoops
+          </option>
+          <option value="soft-serve">soft-serve</option>
+          <option value="toppings">toppings</option>
+          <option value="cones">cones</option>
+        </select>
 
-      <label className='new-product-availability' htmlFor='available'>product available</label>
+        <label className="new-product-availability" htmlFor="available">
+          product available?
+          <select
+          className="product-available-dropdown"
+
+            onChange={e => setAvailable(e.target.value)}
+            value={available}
+          >
+            <option value="false">false</option>
+            <option value="true">true</option>
+          </select>
+        </label>
+
+        {/* 
       <input 
-        onChange={() => bindAvailable(true)} 
+        onChange={() => setAvailable(true)} 
         type='checkbox'
         value={available}
-        />
+        */}
 
-      <div
-        className="add-product-btn"
-        onClick={() => {
-          setNewProduct();
-          resetName();
-          resetP_image();
-          resetPrice();
-          
-        }}
+        <div
+          className="add-product-btn"
+          onClick={() => {
+            setNewProduct();
+            resetName();
+            resetP_image();
+            resetPrice();
+          }}
         >
-        add product
-      </div>
-        </section>
+          add product
+        </div>
+      </section>
     </div>
   );
 }
